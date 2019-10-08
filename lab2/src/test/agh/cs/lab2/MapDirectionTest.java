9287ea910c4604e60978e7223c9dfe5b833b027f
maciektr <maciek2tr99@gmail.com>
2019-10-08 19:18:04 +0200

    lab2
    Solved Lab2

---
+++ lab2/src/test/agh/cs/lab2/MapDirectionTest.java
@@ -0,0 +1,27 @@
+package agh.cs.lab2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
+//red green refactor
+
+public class MapDirectionTest {
+    @Test
+    public void testNext(){
+        Assert.assertEquals(MapDirection.SOUTH, MapDirection.EAST.next());
+        Assert.assertEquals(MapDirection.EAST, MapDirection.NORTH.next());
+        Assert.assertEquals(MapDirection.NORTH, MapDirection.WEST.next());
+        Assert.assertEquals(MapDirection.WEST, MapDirection.SOUTH.next());
+    }
+
+    @Test
+    public void testPrevious(){
+        Assert.assertEquals(MapDirection.SOUTH, MapDirection.WEST.previous());
+        Assert.assertEquals(MapDirection.EAST, MapDirection.SOUTH.previous());
+        Assert.assertEquals(MapDirection.NORTH, MapDirection.EAST.previous());
+        Assert.assertEquals(MapDirection.WEST, MapDirection.NORTH.previous());
+    }
+
+}
